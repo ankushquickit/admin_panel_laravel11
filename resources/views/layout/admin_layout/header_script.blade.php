17e2d463f464464e52ca37081324d351963cc1f2
Ankush Pandey <ankushquickit@users.noreply.github.com>
2024-07-06 17:00:13 +0530

    admin

---
+++ resources/views/layout/admin_layout/header_script.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Admin</title>
+        <link href="{{ asset('for_admin_dashboard/css/bootstrap.min.css') }}" rel="stylesheet">
+        <link href="{{ asset('for_admin_dashboard/font-awesome/css/font-awesome.css') }}" rel="stylesheet">
+        <link href="{{ asset('for_admin_dashboard/css/animate.css') }}" rel="stylesheet">
+        <link href="{{ asset('for_admin_dashboard/css/style.css') }}" rel="stylesheet">
+    </head>
+<body>
